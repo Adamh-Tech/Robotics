c1d4e8e8d37e75c9b66d87659f76366cebd6058c
Adamh-Tech <60155693+Adamh-Tech@users.noreply.github.com>
2020-09-25 20:52:12 -0700

    Add files via upload

---
+++ Assignment_0/Circle/Circle.ino
@@ -0,0 +1,32 @@
+
+#include <Romi32U4.h>
+
+Romi32U4Motors motors;
+Romi32U4ButtonA buttonA;
+
+void setup()
+{
+  // Wait for the user to press button A.
+  buttonA.waitForButton();
+
+  // Delay so that the robot does not move away while the user is
+  // still touching it.
+  delay(1000);
+}
+
+void loop()
+{
+  // Run left motor forward.
+  ledYellow(1);
+  for (int speed = 0; speed <= 100; speed++)
+  {
+    motors.setLeftSpeed(speed);
+  }
+
+  // Run right motor forward.
+  ledRed(1);
+  for (int speed = 0; speed <= 200; speed++)
+  {
+    motors.setRightSpeed(speed);
+  }
+}
